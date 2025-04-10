9aeb9c5ca57bc2bbc2b45354d3adc83e08d129f2
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-10 15:36:23 -0300

    using @ComponentScan in the configuration class

---
+++ src/main/java/main/configuration/ProjectConfiguration.java
@@ -0,0 +1,9 @@
+package main.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration  // marks the configuration class
+@ComponentScan(basePackages = {"proxies", "services", "repositories"})
+public class ProjectConfiguration {
+}
