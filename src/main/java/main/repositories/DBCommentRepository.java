b17e9d2da2c9d0bb6309ab0d61931f0f2f0b2a10
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-10 14:54:20 -0300

    implementing the CommentRepository interface

---
+++ src/main/java/main/repositories/DBCommentRepository.java
@@ -0,0 +1,12 @@
+package main.repositories;
+
+import main.model.Comment;
+
+public class DBCommentRepository implements CommentRepository {
+
+    @Override
+    public void storeComment(Comment comment) {
+        System.out.println("Storing comment: " + comment.getText());
+    }
+
+}
