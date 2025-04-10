7a21497c6bff7b02547baa1f2d4d4b637ec8f240
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-10 15:31:01 -0300

    making the CommentService class a component

--- src/main/java/main/services/CommentService.java
+++ src/main/java/main/services/CommentService.java
@@ -3,21 +3,26 @@ package main.services;
 import main.model.Comment;
 import main.proxies.CommentNotificationProxy;
 import main.repositories.CommentRepository;
+import org.springframework.stereotype.Component;
 
+@Component  // Spring creates a bean of this class and adds it to its context.
 public class CommentService {
-    // we define the two dependencies as attributes of the class
+    // we define the two dependencies as attributes of the class.
     private final CommentRepository commentRepository;
 
-    // we provide the dependencies when the object is built through the parameters of the constructor
+    // we provide the dependencies when the object is built through the parameters of the constructor.
     private final CommentNotificationProxy commentNotificationProxy;
 
+    // we just use @Autowired if the class had more than one constructor.
+    // Spring uses this constructor to create the bean and  injects references
+    // from its context in the parameters when creating the instance.
     public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
 
     // we implement the use case that delegates the "store comment" and
-    // "send notification" responsibilities to the dependencies
+    // "send notification" responsibilities to the dependencies.
     public void publishComment(Comment comment) {
         commentRepository.storeComment(comment);
         commentNotificationProxy.sendComment(comment);
