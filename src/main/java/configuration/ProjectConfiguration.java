ef627556a4c34007b587d117d981ac4a20a36e0e
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-10 16:16:43 -0300

    removing the unusual imports

--- src/main/java/configuration/ProjectConfiguration.java
+++ src/main/java/configuration/ProjectConfiguration.java
@@ -1,7 +1,6 @@
 package configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration;
 import proxies.CommentNotificationProxy;
 import proxies.EmailCommentNotificationProxy;
--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,11 +1,8 @@
 package services;
 
 import model.Comment;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
-import org.springframework.stereotype.Component;
 
 public class CommentService {
 
