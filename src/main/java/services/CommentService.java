875177afd588f224aee63ad87119c49f4b6219fb
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-10 16:10:10 -0300

    dependency injection with abstractions

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,31 +1,17 @@
 package services;
 
 import model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
 import org.springframework.stereotype.Component;
 
-@Component  // Spring creates a bean of this class and adds it to its context.
+@Component
+@ComponentScan(
+        basePackages = {"proxies", "services", "repositories"}
+)
 public class CommentService {
-    // we define the two dependencies as attributes of the class.
-    private final CommentRepository commentRepository;
-
-    // we provide the dependencies when the object is built through the parameters of the constructor.
-    private final CommentNotificationProxy commentNotificationProxy;
-
-    // we just use @Autowired if the class had more than one constructor.
-    // Spring uses this constructor to create the bean and  injects references
-    // from its context in the parameters when creating the instance.
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
-        this.commentRepository = commentRepository;
-        this.commentNotificationProxy = commentNotificationProxy;
-    }
-
-    // we implement the use case that delegates the "store comment" and
-    // "send notification" responsibilities to the dependencies.
-    public void publishComment(Comment comment) {
-        commentRepository.storeComment(comment);
-        commentNotificationProxy.sendComment(comment);
-    }
+    
 
 }
