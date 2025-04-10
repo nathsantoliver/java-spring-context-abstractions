821db96564f33df56a3d6850af93ac8475148d0c
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-10 16:12:26 -0300

    dependency injection with abstractions

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -7,11 +7,20 @@ import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
 import org.springframework.stereotype.Component;
 
-@Component
-@ComponentScan(
-        basePackages = {"proxies", "services", "repositories"}
-)
 public class CommentService {
-    
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository,
+                          CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment) {
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
 
 }
