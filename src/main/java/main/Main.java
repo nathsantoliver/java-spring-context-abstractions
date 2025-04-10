a892bcc520b0db64ad49143fe263c10f753fd3f3
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-10 15:18:45 -0300

    calling the use case in the Main class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,28 @@
 package main;
 
+import main.model.Comment;
+import main.proxies.EmailCommentNotificationProxy;
+import main.repositories.DBCommentRepository;
+import main.services.CommentService;
+
 public class Main {
     public static void main(String[] args) {
-        //
+        // 1. creates the instance for the dependencies
+        var commentRepository = new DBCommentRepository();
+        var commentNotificationProxy = new EmailCommentNotificationProxy();
+
+        // 2. creates the instance of the service class and providing the dependencies
+        var commentService = new CommentService(commentRepository, commentNotificationProxy);
+
+        // 3. creates an instance of comment to send as a parameter to the publish comment use case
+        var comment = new Comment();
+        comment.setAuthor("Laurentiu");
+
+        // 4. calls the publish comment use case
+        comment.setText("Demo comment");
+
+        commentService.publishComment(comment);
+
+
     }
 }
