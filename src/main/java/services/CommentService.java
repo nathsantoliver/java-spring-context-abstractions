ea29ed48c6c710746cd914e59025b1a7a328182c
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-10 16:32:16 -0300

    specifying the implementation Spring needs to inject with @Qualifier

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,6 +1,7 @@
 package services;
 
 import model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
@@ -9,10 +10,13 @@ import repositories.CommentRepository;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+
     private final CommentNotificationProxy commentNotificationProxy;
 
+    // for each parameter where we want to use a specific implementation,
+    // we annotate the parameter @Qualifier.
     public CommentService(CommentRepository commentRepository,
-                          CommentNotificationProxy commentNotificationProxy) {
+                          @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
